ae27f55b11645415d299f39e3d3cdd9280aa5508
Bergschaf <christian.k@netcom-mail.de>
2025-01-28 21:27:31 +0100

    uuu

--- Aufgabe_2/Aufgabe_2.py
+++ Aufgabe_2/Aufgabe_2.py
@@ -4,7 +4,25 @@ from PIL import Image
 # TODO beweis, dass das NP-schwer ist?
 
 
-filename = "Examples/labyrinthe5.txt"
+filename = "Examples/labyrinthe7.txt"
+
+
+class Instruction:
+    UP = "UP"
+    DOWN = "DOWN"
+    LEFT = "LEFT"
+    RIGHT = "RIGHT"
+    @staticmethod
+    def move(x, y, instruction):
+        if instruction == Instruction.UP:
+            return x, y - 1
+        if instruction == Instruction.DOWN:
+            return x, y + 1
+        if instruction == Instruction.LEFT:
+            return x - 1, y
+        if instruction == Instruction.RIGHT:
+            return x + 1, y
+        raise ValueError("Invalid instruction")
 
 
 class Labyrinth:
@@ -21,9 +39,9 @@ class Labyrinth:
         returns the number of lines parsed
         """
         n, m = self.n, self.m
-        #vertical_walls = []  # if there is a wall to the right of (i,j), then vertical_walls[i][j] = 1
-        #horizontal_walls = []  # if there is a wall below (i,j), then horizontal_walls[i][j] = 1
-        #holes = []  # list of holes
+        # vertical_walls = []  # if there is a wall to the right of (i,j), then vertical_walls[i][j] = 1
+        # horizontal_walls = []  # if there is a wall below (i,j), then horizontal_walls[i][j] = 1
+        # holes = []  # list of holes
 
         for m_ in range(m):
             self.vertical_walls.append(list(map(int, data[m_].split())))
@@ -33,19 +51,22 @@ class Labyrinth:
 
         num_holes = int(data[2 * m - 1])
         for n in range(num_holes):
-            self.holes.append(list(map(int, data[2 * m + n].split())))
+            self.holes.append(tuple(map(int, data[2 * m + n].split())))
         return 2 * m + num_holes
 
     def visualize(self, path, filename="test.png"):
-        img = np.ones((self.m * 20 + 1, self.n * 20, 3), dtype=np.uint8) * 255
-        def draw_rectangle(img, x, y, widht, height, color=(0, 255, 0)):
+        img = np.ones((self.m * 20 + 1, self.n * 20, 3), dtype=np.uint8) * 0  #255
+
+        def draw_rectangle(img, x, y, widht, height, color=(0, 255, 0), alpha=0.3):
+            color = (int(color[0] * alpha), int(color[1] * alpha), int(color[2] * alpha))
             for i in range(x, x + widht):
                 for j in range(y, y + height):
                     if i < img.shape[1] and j < img.shape[0]:
-                        img[j, i] = color
+                        img[j, i] = color + img[j, i]
             return img
 
-        def draw_line(img, x0, y0, x1, y1, color=(0, 0, 0)):
+        def draw_line(img, x0, y0, x1, y1, color=(0, 0, 0),alpha=0.3):
+            color = (int(color[0] * alpha), int(color[1] * alpha), int(color[2] * alpha))
             dx = x1 - x0
             dy = y1 - y0
             if abs(dx) > abs(dy):
@@ -55,7 +76,7 @@ class Labyrinth:
                 for x in range(x0, x1):
                     y = y0 + dy * (x - x0) // dx
                     if y < img.shape[0] and y >= 0 and x < img.shape[1] and x >= 0:
-                        img[y, x] = color
+                        img[y, x] = color + img[y, x]
             else:
                 if y0 > y1:
                     x0, x1 = x1, x0
@@ -63,25 +84,24 @@ class Labyrinth:
                 for y in range(y0, y1):
                     x = x0 + dx * (y - y0) // dy
                     if y < img.shape[0] and y >= 0 and x < img.shape[1] and x >= 0:
-                        img[y, x] = color
+                        img[y, x] = color + img[y, x]
             return img
-
 
         # Draw vertical walls
         for j in range(self.m):
-            for i in range(self.n-1):
+            for i in range(self.n - 1):
                 if self.vertical_walls[j][i] == 1:
-                    img = draw_rectangle(img, (i+1) * 20 - 3, j * 20, 5, 20)
+                    img = draw_rectangle(img, (i + 1) * 20 - 3, j * 20, 5, 20, alpha=0.3)
 
         # Draw horizontal walls
         for m_ in range(self.m - 1):
             for i in range(self.n):
                 if self.horizontal_walls[m_][i] == 1:
-                    img = draw_rectangle(img, i * 20, (m_ + 1) * 20 - 3, 20, 5)
+                    img = draw_rectangle(img, i * 20, (m_ + 1) * 20 - 3, 20, 5, alpha=0.3)
 
         # Draw holes
         for hole in self.holes:
-            img = draw_rectangle(img, hole[0] * 20, hole[1] * 20,20, 20, (255, 0, 0))
+            img = draw_rectangle(img, hole[0] * 20, hole[1] * 20, 20, 20, (255, 0, 0))
 
         # Draw path
         for ii, (i, j) in enumerate(path):
@@ -95,37 +115,51 @@ class Labyrinth:
         img.save(filename)
 
 
+
+    def is_wall_below(self, x, y):
+        if y == self.m - 1:
+            return True
+        return self.horizontal_walls[y][x] == 1
+
+    def is_wall_above(self, x, y):
+        if y == 0:
+            return True
+        return self.horizontal_walls[y - 1][x] == 1
+
+    def is_wall_right(self, x, y):
+        if x == self.n - 1:
+            return True
+        return self.vertical_walls[y][x] == 1
+
+    def is_wall_left(self, x, y):
+        if x == 0:
+            return True
+        return self.vertical_walls[y][x - 1] == 1
+
     def get_reachable_neighbours(self, x, y):
         possible = []
 
-        # UP
-        if y > 0 and self.horizontal_walls[y - 1][x] == 0:
-            possible.append((x, y - 1))
-
-        # DOWN
-        if y < self.m - 1 and self.horizontal_walls[y][x] == 0:
+        if not self.is_wall_below(x, y):
             possible.append((x, y + 1))
-
-        # LEFT
-        if x > 0 and self.vertical_walls[y][x - 1] == 0:
-            possible.append((x - 1, y))
-
-        # RIGHT
-        if x < self.n - 1 and self.vertical_walls[y][x] == 0:
+        if not self.is_wall_above(x, y):
+            possible.append((x, y - 1))
+        if not self.is_wall_right(x, y):
             possible.append((x + 1, y))
-
+        if not self.is_wall_left(x, y):
+            possible.append((x - 1, y))
         for p in possible:
+            if p == (18, 6):
+                print("test")
             if p in self.holes:
                 possible.remove(p)
                 # TODO wahrscheinlich nicht optimal
         return possible
 
-
     def get_best_path_dijkstra(self):
         start = (0, 0)
         end = (self.n - 1, self.m - 1)
         stack = [(start, 0, None)]
-        visited = {start : None}
+        visited = {start: None}
         while stack:
             stack = sorted(stack, key=lambda x: x[1])
             current, dist, previous = stack.pop(0)
@@ -142,12 +176,108 @@ class Labyrinth:
                             break
                     else:
                         stack.append((neighbour, dist + 1, current))
+
+        if end not in visited:
+            raise Exception("No path found")
+
         path = [end]
         current = visited[end]
         while current:
             path.append(current)
             current = visited[current]
-        return path
+        return path[::-1]
+
+    def get_instructions_from_path(self, path):
+        """
+        :return: [(instruction, [every instruction that would go against a wall])]
+        TODO dass muss eig nicht sein, dass kann direkt während der Suche passieren
+        """
+        instructions = []
+        for i in range(1, len(path)):
+            x0, y0 = path[i - 1]
+            impossible = []
+            if self.is_wall_left(x0, y0):
+                impossible.append(Instruction.LEFT)
+            if self.is_wall_right(x0, y0):
+                impossible.append(Instruction.RIGHT)
+            if self.is_wall_above(x0, y0):
+                impossible.append(Instruction.UP)
+            if self.is_wall_below(x0, y0):
+                impossible.append(Instruction.DOWN)
+
+            x1, y1 = path[i]
+            if x1 > x0:
+                instructions.append((Instruction.RIGHT, impossible))
+            elif x1 < x0:
+                instructions.append((Instruction.LEFT, impossible))
+            elif y1 > y0:
+                instructions.append((Instruction.DOWN, impossible))
+            elif y1 < y0:
+                instructions.append((Instruction.UP, impossible))
+
+        return instructions
+
+
+    def move(self, x, y, instruction):
+        if instruction == Instruction.UP:
+            if self.is_wall_above(x, y):
+                return (x, y)
+        elif instruction == Instruction.DOWN:
+            if self.is_wall_below(x, y):
+                return (x, y)
+        elif instruction == Instruction.LEFT:
+            if self.is_wall_left(x, y):
+                return (x, y)
+        elif instruction == Instruction.RIGHT:
+            if self.is_wall_right(x, y):
+                return (x, y)
+        new_pos = Instruction.move(x, y, instruction)
+        if new_pos in self.holes:
+            return (0,0)
+        return new_pos
+
+
+def get_double_path_dijkstra(l1, l2):
+    start = ((0, 0), (0, 0))
+    n, m = l1.n, l1.m
+    assert n == l2.n and m == l2.m
+    end = ((n - 1, m - 1), (n - 1, m - 1))
+
+    # TODO ab jetzt wirds ineffizient
+    stack = [(start, 0, None, None)]
+    visited = {start: (None, None)}
+    while stack:
+        stack = sorted(stack, key=lambda x: x[1])
+        current, dist, previous, instruction = stack.pop(0)
+
+        visited[current] = (previous, instruction)
+        if current == end:
+            break
+
+        for direction in [Instruction.DOWN, Instruction.LEFT, Instruction.UP, Instruction.RIGHT]:
+            new_pos = (l1.move(*current[0], direction), l2.move(*current[1], direction))
+            if new_pos == current:
+                continue
+
+            if new_pos not in visited.keys():
+                for i in range(len(stack)):
+                    if new_pos == stack[i][0]:
+                        if dist + 1 < stack[i][1]:
+                            stack[i] = (new_pos, dist + 1, current, direction)
+                        break
+                else:
+                    stack.append((new_pos, dist + 1, current, direction))
+    path1 = [end[0]]
+    path2 = [end[1]]
+    instructions = []
+    current, inst = visited[end]
+    while current:
+        instructions.append(inst)
+        path1.append(current[0])
+        path2.append(current[1])
+        current, inst = visited[current]
+    return path1[::-1], path2[::-1], instructions[::-1]
+
 
 
 def main(filename):
@@ -156,12 +286,22 @@ def main(filename):
         n, m = map(int, data.pop(0).split())
         l1 = Labyrinth(n, m)
         offset = l1.parse(data)
-
-
-        l1.visualize([])
         l2 = Labyrinth(n, m)
         l2.parse(data[offset:])
-        l2.visualize(l2.get_best_path_dijkstra(), "test2.png")
-        l1.visualize(l1.get_best_path_dijkstra(), "test1.png")
+    l1.visualize([], filename="test1.png")
+    l2.visualize([], filename="test2.png")
+    print(l2.get_best_path_dijkstra())
+    exit()
+
+    path1, path2, inst = get_double_path_dijkstra(l1, l2)
+    l1.visualize(path1, filename="test1.png")
+    l2.visualize(path2, filename="test2.png")
+    print(path1)
+    print(path2)
+    print(inst)
+
+
+
+
 if __name__ == '__main__':
     main(filename)
